a19e4bda365570063cb4849676f2a516a71fb79d
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-07 14:55:35 -0300

    Realização da atividade 2 do Bloco 1

---
+++ Modulo 4/Bloco 1/atividade2.js
@@ -0,0 +1,11 @@
+const inteiro = 10;
+const decimal = 2.0;
+const meuNome = "Frederico";
+const boleano = true;
+let roberto ; 
+
+console.log(typeof inteiro)
+console.log(typeof decimal)
+console.log(typeof meuNome)
+console.log(typeof boleano)
+console.log(typeof roberto)
