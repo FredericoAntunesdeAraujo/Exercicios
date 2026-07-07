55762d438d823e69f6922933384a9bae8624324c
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-07 14:36:58 -0300

    Alteração no nome de arquivos, realização da atividade 1 do bloco 1 de JS

---
+++ Modulo 4/Bloco 1/atividade1.js
@@ -0,0 +1,10 @@
+const nome = "Ana";
+nome = "Carlos";
+
+let idade = 25;
+let idade = 30;
+
+console.log(nome, idade);
+
+// código da erro por ter duas variáveis de idade, além de ter uma constante sendo chamada duas vezes
+// é isso ai
