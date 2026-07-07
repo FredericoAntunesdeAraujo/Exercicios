4677b915f58bf50dca2973a35f7dfefada7c4808
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-07 15:37:57 -0300

    Realização da atividade 5 do Bloco 1 de JS

---
+++ Modulo 4/Bloco 1/atividade5.js
@@ -0,0 +1,11 @@
+const primeiroNome = "João";
+const dataNascimento = "01/01/2000";
+const salario = 3000;
+let numerodeTentativas = 0;
+const nomeCompleto = "João Silva";
+
+// primeiro_nome = errado pq ta errado
+// DataNascimento = PascalCase é para classes e construtores
+// SALARIO = apenas para constantes globais de configuração
+// numerodetentativas = não usa camelCase
+// NomeCompleto = PascalCase é para classes e construtores
