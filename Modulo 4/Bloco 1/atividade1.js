37409947ce7ef0cf805b4870381cf0140fd98e46
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-07 14:47:34 -0300

    Arrumando erro da atividade 1 do bloco 1

--- Modulo 4/Bloco 1/atividade1.js
+++ Modulo 4/Bloco 1/atividade1.js
@@ -1,10 +1,10 @@
-const nome = "Ana";
+let  nome = "Ana";
 nome = "Carlos";
 
-let idade = 25;
-let idade = 30;
+let  idade = 25;
+idade = 30;
 
 console.log(nome, idade);
 
 // código da erro por ter duas variáveis de idade, além de ter uma constante sendo chamada duas vezes
-// é isso ai
+// para arrumar tem q trocar os dois pra let
