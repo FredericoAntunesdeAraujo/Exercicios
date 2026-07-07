c387faa02dd1fdc1392ee833f6663bf3c240061f
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-07 16:58:47 -0300

    Realizando a atividade 2 do bloco 2 de JS

---
+++ Modulo 4/Bloco 2/atividade2.js
@@ -0,0 +1,11 @@
+let nota = 10;
+
+if ( nota >= 9) {
+    console.log("Excelente")
+} else if (nota >=7){
+    console.log("Aprovado")
+} else if (nota >= 4){
+    console.log("Reprovado")
+} else {
+    console.log("Ou você é muito ruim, ou você não fez a prova.")
+}
