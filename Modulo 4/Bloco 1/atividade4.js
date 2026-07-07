e3a8f06491e342820b49416059bf2bc111f37bf1
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-07 15:30:46 -0300

    Realização da atividade 4

---
+++ Modulo 4/Bloco 1/atividade4.js
@@ -0,0 +1,12 @@
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(0 == false);
+console.log(0 === false);
+console.log(null == undefined);
+console.log(null === undefined);
+
+// O Código deve exibir 1 = false, 2 = true, 3 = true, 4 = false, 5 = false, 6 = true.
+
+// Pós teste : 1 = true, 2 = false, 3 = true, 4 = false, 5 = true, 6 = false.
+
+// Acertei 2 lets go
