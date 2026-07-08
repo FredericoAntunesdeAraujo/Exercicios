c7e692a3d9defaedc1e14368ff0d94b26e3283d1
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-08 14:48:19 -0300

    Realização da atividade 6 do bloco 2 de JS

---
+++ Modulo 4/Bloco 2/atividade6.js
@@ -0,0 +1,14 @@
+let numero = 1;
+
+// do {
+//     console.log(`${numero}`)
+//     numero++
+// }
+// while (numero < 11)
+
+do {
+    if (numero % 2 !== 0) {
+        console.log(numero);
+    }
+    numero++;
+} while (numero < 22);
