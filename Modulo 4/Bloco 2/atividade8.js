33e0b46edadf277963e3bad5a6f75a97cd823c32
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-08 15:59:01 -0300

    Realização da atividade 8 do bloco 2 de JS

---
+++ Modulo 4/Bloco 2/atividade8.js
@@ -0,0 +1,14 @@
+const frutas = ["Maçã", "Banana", "Laranja"];
+
+frutas.push("Uva");
+console.log(frutas);
+
+frutas.unshift("Morango");
+console.log(frutas);
+
+const removida = frutas.pop();
+console.log(removida);
+
+const eRemovida = frutas.shift();
+console.log(eRemovida);
+
