ff1ef9329f8c6ffd8a08039c09db583417e24930
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-08 14:26:57 -0300

    Realização da atividade 5 do bloco 2 de JS

---
+++ Modulo 4/Bloco 2/atividade5.js
@@ -0,0 +1,7 @@
+const tecnologias = ["HTML", "CSS", "JavaScript", "Bootstrap", "Node.js"];
+let numero = 1
+
+for (const tecnologia of tecnologias ){
+    console.log(`${numero} ${tecnologia}`);
+    numero++
+}
