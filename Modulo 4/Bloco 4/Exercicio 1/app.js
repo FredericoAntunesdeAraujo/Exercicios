b6138f8348356e6280e9cf4afac856cf8bb16bc8
Frederico Antunes <antunesfrederico06@gmail.com>
2026-07-14 18:37:55 -0300

    Realização da atividade 1 de DOM, JS bloco 4

---
+++ Modulo 4/Bloco 4/Exercicio 1/app.js
@@ -0,0 +1,9 @@
+const tituloNovo = document.querySelector('#titulo');
+const paragrafo = document.querySelector('.descricao');
+const botao = document.querySelector('button')
+
+
+
+console.log(tituloNovo.textContent);
+console.log(paragrafo.textContent);
+console.log(botao.tagName);
