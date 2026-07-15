c2f7f842ce504be7906017575805fa042a277f23
FREDERICO ANTUNES DE ARAUJO <frederico_araujo@estudante.sesisenai.org.br>
2026-07-15 14:06:39 -0300

    Realização da atividade 5 do bloco 4 de JS DOM

---
+++ Modulo 4/Bloco 4/Exercicio 5/app.js
@@ -0,0 +1,19 @@
+const minhaLista = document.querySelector("#lista");
+
+const iten1 = document.createElement("li");
+iten1.textContent = "C#";
+
+const iten2 = document.createElement("li");
+iten2.textContent = "PostgreSQL";
+
+const iten3 = document.createElement("li");
+iten3.textContent = "Python";
+
+const iten4 = document.createElement("li");
+iten4.textContent = "CSS";
+
+
+minhaLista.appendChild(iten1);
+minhaLista.appendChild(iten2);
+minhaLista.appendChild(iten3);
+minhaLista.appendChild(iten4);
