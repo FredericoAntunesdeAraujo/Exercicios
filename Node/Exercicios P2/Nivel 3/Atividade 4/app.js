684db3dca20974b6e4991cae3c4960aa123860f2
Frederico Antunes <antunesfrederico06@gmail.com>
2026-08-16 20:52:25 -0300

    Ajuste na atividade 4 de Node

---
+++ Node/Exercicios P2/Nivel 3/Atividade 4/app.js
@@ -0,0 +1,7 @@
+const caminho = require('./caminhos');
+
+console.log("==========================");
+console.log(caminho.montarCaminho("caminhos.js"));
+console.log(caminho.montarCaminho("arquivo2.js"));
+console.log(caminho.montarCaminho("arquivo3.js"));
+console.log("==========================");
--- Node/Exercicios P2/Nivel 3/Atividade 4/caminhos.js
+++ Node/Exercicios P2/Nivel 3/Atividade 4/caminhos.js
@@ -4,8 +4,9 @@ function montarCaminho(nomeArquivo) {
     return path.join(__dirname, 'Atividade 4', nomeArquivo);
 }
 
-console.log("==========================");
-console.log(montarCaminho("caminhos.js"));
-console.log(montarCaminho("arquivo2.js"));
-console.log(montarCaminho("arquivo3.js"));
-console.log("==========================");
+
+
+
+module.exports = {
+    montarCaminho
+}
