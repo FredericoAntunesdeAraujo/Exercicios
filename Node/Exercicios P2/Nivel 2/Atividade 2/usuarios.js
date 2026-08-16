d2c20016b9043306d518d83a60bb4a8418e25029
Frederico Antunes <antunesfrederico06@gmail.com>
2026-08-16 19:25:28 -0300

    Finalização do exercício 2 do nivel 2 da parte 2 de Node

--- Node/Exercicios P2/Nivel 2/Atividade 2/usuarios.js
+++ Node/Exercicios P2/Nivel 2/Atividade 2/usuarios.js
@@ -1,22 +1,19 @@
 let usuarios = [];
 
-let usuario = {
-    nome : "",
-    idade : 0
+function criarUsuario(nome, idade) {
+    let usuario = {
+        nome: String(nome),
+        idade: Number(idade)
+    };
+
+    usuarios.push(usuario);
 }
 
-function criarUsuario(nome,idade){
-    usuario.nome = String(nome);    
-    usuario.idade = Number(idade);
-    
-    usuarios.push(usuario);
-};
-
-function listarUsuario(){
+function listarUsuario() {
     console.log(usuarios);
 }
 
 module.exports = {
     criarUsuario,
     listarUsuario
-}
+};
